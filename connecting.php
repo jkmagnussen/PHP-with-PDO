6e1d5a0fd82a9a1ed553616740ea6c91956d6b25
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-12-03 23:17:23 +0000

    connection functional

--- connecting.php
+++ connecting.php
@@ -9,3 +9,9 @@ include 'config.php';
 // second argument is user and third argument is password. 
 
 $con = new PDO('mysql:host='.DB_HOST.'dbname='.DB_NAME.'port=80', DB_USER, DB_PASS);
+
+// to find out what drivers are available, use method on PDO:: 
+
+$drivers = PDO::getAvailableDrivers();
+
+var_dump($drivers);
